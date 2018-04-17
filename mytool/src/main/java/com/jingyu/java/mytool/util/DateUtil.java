b8d55a82f2f3395927fd26b7a9aa066fccd71ba4
jingyu <fengjingyu@foxmail.com>
2018-04-17 12:28:42 +0800

    DateUtil

--- mytool/src/main/java/com/jingyu/java/mytool/util/DateUtil.java
+++ mytool/src/main/java/com/jingyu/java/mytool/util/DateUtil.java
@@ -101,6 +101,26 @@ public class DateUtil {
     }
 
     /**
+     * 日期转星期
+     */
+    public static String dateToWeek(String datetime, String pattern) {
+        SimpleDateFormat f = new SimpleDateFormat(pattern);
+        String[] weekDays = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+        Calendar cal = Calendar.getInstance(); // 获得一个日历
+        Date datet = null;
+        try {
+            datet = f.parse(datetime);
+            cal.setTime(datet);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        int w = cal.get(Calendar.DAY_OF_WEEK) - 1; // 指示一个星期中的某天。
+        if (w < 0)
+            w = 0;
+        return weekDays[w];
+    }
+
+    /**
      * 指定日期加上天数后的日期
      */
     public static Date plusDay(int days, Date date, String pattern) {
