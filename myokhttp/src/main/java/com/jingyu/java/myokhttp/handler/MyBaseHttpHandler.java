b144f99d3a91bb57f9e234d3764c9a5128264ef8
jingyu <fengjingyu@foxmail.com>
2018-05-04 10:07:03 +0800

    V0.2.2 myokhttp

--- myokhttp/src/main/java/com/jingyu/java/myokhttp/handler/MyBaseHttpHandler.java
+++ myokhttp/src/main/java/com/jingyu/java/myokhttp/handler/MyBaseHttpHandler.java
@@ -8,9 +8,6 @@ import com.jingyu.java.mytool.util.IOUtil;
 
 import java.io.File;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author fengjingyu@foxmail.com
@@ -25,30 +22,6 @@ public abstract class MyBaseHttpHandler<T> implements IMyHttpHandler<T> {
         return myReqInfo;
     }
 
-    public String parse2String(MyReqInfo myReqInfo, MyRespInfo myRespInfo, InputStream inputStream) {
-        // 只能读一次，否则异常
-        try {
-            myRespInfo.setDataBytes(IOUtil.getBytes(inputStream));
-            return new String(myRespInfo.getDataBytes(), Constants.UTF8);
-        } catch (Exception e) {
-            throw new RuntimeException(myReqInfo.getUrl() + MyHttpCallBack.LINE + e);
-        }
-    }
-
-    public File parse2File(MyReqInfo myReqInfo, InputStream inputStream, File file) {
-        if (IOUtil.inputStream2File(inputStream, file)) {
-            return file;
-        } else {
-            throw new RuntimeException("inputStream2file异常 " + MyHttpCallBack.LINE + "file = " + file + MyHttpCallBack.LINE + myReqInfo.getUrl());
-        }
-    }
-
-    //TODO 实际项目里需要比对接口业务的状态码; 如果请求一般的浏览网页就无需匹配状态码，默认返回true
-    @Override
-    public boolean onMatchAppCode(MyReqInfo myReqInfo, MyRespInfo myRespInfo, T resultBean) {
-        return true;
-    }
-
     @Override
     public void onUploadProgress(long bytesWritten, long totalSize) {
 
@@ -57,6 +30,12 @@ public abstract class MyBaseHttpHandler<T> implements IMyHttpHandler<T> {
     @Override
     public void onParseException(MyReqInfo myReqInfo, MyRespInfo myRespInfo) {
 
+    }
+
+    //TODO 实际项目里需要比对接口业务的状态码; 如果请求一般的浏览网页就无需匹配状态码，默认返回true
+    @Override
+    public boolean onMatchAppCode(MyReqInfo myReqInfo, MyRespInfo myRespInfo, T resultBean) {
+        return true;
     }
 
     @Override
@@ -77,5 +56,23 @@ public abstract class MyBaseHttpHandler<T> implements IMyHttpHandler<T> {
     @Override
     public void onFinally(MyReqInfo myReqInfo, MyRespInfo myRespInfo) {
 
+    }
+
+    public String parse2String(MyReqInfo myReqInfo, MyRespInfo myRespInfo, InputStream inputStream) {
+        // 只能读一次，否则异常
+        try {
+            myRespInfo.setDataBytes(IOUtil.getBytes(inputStream));
+            return new String(myRespInfo.getDataBytes(), Constants.UTF8);
+        } catch (Exception e) {
+            throw new RuntimeException(myReqInfo.getUrl() + MyHttpCallBack.LINE + e);
+        }
+    }
+
+    public File parse2File(MyReqInfo myReqInfo, InputStream inputStream, File file) {
+        if (IOUtil.inputStream2File(inputStream, file)) {
+            return file;
+        } else {
+            throw new RuntimeException("inputStream2file异常 " + MyHttpCallBack.LINE + "file = " + file + MyHttpCallBack.LINE + myReqInfo.getUrl());
+        }
     }
 }
