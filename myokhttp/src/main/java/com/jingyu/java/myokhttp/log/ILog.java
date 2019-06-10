af5fc5eb4380be4c83f3223337e7f55fdc4aaae6
jingyu <fengjingyu@foxmail.com>
2019-06-10 23:08:54 +0800

    ..

--- myokhttp/src/main/java/com/jingyu/java/myokhttp/log/ILog.java
+++
@@ -1,8 +0,0 @@
-package com.jingyu.java.myokhttp.log;
-
-public interface ILog {
-
-    void i(String tag, String msg);
-
-    void e(String tag, String msg, Exception e);
-}
--- myokhttp/src/main/java/com/jingyu/java/myokhttp/req/ReqType.java
+++
@@ -1,9 +0,0 @@
-package com.jingyu.java.myokhttp.req;
-
-/**
- * @author fengjingyu@foxmail.com
- *  请求类型
- */
-public enum ReqType {
-    GET, POST
-}
